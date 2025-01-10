7f38b68c4b6fef3e0a5dcaf014f6f69f1d0fdccf
PolloLopez <pollolopeza@gmail.com>
2025-01-10 20:03:23 -0300

    navbar

--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../../components/Navbar/NavBar";
+import Navbar from "../../components/Navbar/navbar";
 import Footer from "../../components/Footer/Footer";
 import "./MainLayout.css";
 
