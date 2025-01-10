1e3a11001c3b4477a47fc6d5f8842fefcdf3169c
PolloLopez <pollolopeza@gmail.com>
2025-01-10 19:15:51 -0300

    NavBar OK - Footer Ok - Faltan contacto y redes Footer - y estilo a carrito Tutoriales OK

--- src/pages/Tutoriales/tutoriales.jsx
+++ src/pages/Tutoriales/tutoriales.jsx
@@ -4,7 +4,7 @@ const Tutoriales = () => {
     return (
         <div>
             <h1>Videos Tutoriales</h1>
-            <p>Aquí encontrarás varios videos tutoriales para entenderte mejor con tu perriamigo.</p>
+            <p>Aquí encontrarás varios videos tutoriales para entenderte mejor con tu perriamigo. Como asi tambien recetas y juegos</p>
         </div>
     );
 };
