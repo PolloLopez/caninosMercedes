b1275cb105f7b7b4357044089dc2348c4c28b111
PolloLopez <pollolopeza@gmail.com>
2025-01-10 19:43:28 -0300

    acartContex

--- src/pages/Tutoriales/Tutorials.jsx
+++ src/pages/Tutoriales/Tutorials.jsx
@@ -4,7 +4,7 @@ const Tutorials = () => {
     return (
         <div>
             <h1>Videos Tutoriales</h1>
-            <p>Aquí encontrarás varios videos tutoriales para entenderte mejor con tu perriamigo. Como asi tambien recetas y juegos</p>
+            <p>Aquí encontrarás varios videos tutoriales para entenderte mejor con tu perriamigo. Como asi tambien algunas recetas y juegos</p>
         </div>
     );
 };
