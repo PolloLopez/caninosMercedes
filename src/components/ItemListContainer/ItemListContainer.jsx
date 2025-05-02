764a4afaf8276a50078679a8ae8b5e918255b817
PolloLopez <pollolopeza@gmail.com>
2025-05-02 10:17:52 -0300

    Netlify actualizado con buid y Node 22

--- src/components/ItemListContainer/ItemListContainer.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/ItemListContainer/ItemListContainer.jsx
-import React from "react";
-
-const ItemListContainer = () => {
-  return <p>ItemListContainer (vacío temporalmente)</p>;
-};
-
-export default ItemListContainer;
